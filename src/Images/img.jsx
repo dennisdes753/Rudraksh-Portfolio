51ec6a68ae3a168ab6abd2b6eb8284c9ff400937
dennisdes753 <dennisdes753@gmail.com>
2024-01-12 01:44:53 +0530

    section done - about, projects

--- src/Images/img.jsx
+++ src/Images/img.jsx
@@ -1,3 +1,8 @@
 export {default as BannerImage} from './rudraksh_banner.png';
 export { default as Logo } from './Logo_Rudraksh.png';
 export { default as ProfilePic } from './rudrakshpic.png';
+export { default as ProjectImage } from './projectImage.png';
+export { default as FigmaBackground } from './figma_background.png';
+export { default as AdobeBackground } from './adobe-background.png';
+export { default as WebflowBackground } from './webflow_background.png';
+export { default as SketchBackground } from './sketch_background.png';
